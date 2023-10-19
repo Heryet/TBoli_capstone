e516e4d546655a62afd9b96491cd31e69e46cc78
julianne060701 <fernandez.226952@gensan.sti.edu.ph>
2023-10-19 23:38:24 +0800

    remove learner access lesson

--- learnerSideMenu.php
+++ learnerSideMenu.php
@@ -9,12 +9,6 @@
         </a>
     </li>
 
-    <li class="side-nav-item">
-        <a href="learner_access_lesson.php" class="side-nav-link">
-            <i class="uil-notebooks"></i>
-            <span>Access Lesson</span>
-        </a>
-    </li>
 
     <li class="side-nav-item">
         <a href="learner_answer_activity.php" class="side-nav-link">
